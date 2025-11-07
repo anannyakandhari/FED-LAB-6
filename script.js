ea23fbbb721ee6b77237a8aa6eb90ca16edfc5e5
Anannyakandhari <anannyakandhari@gmail.com>
2025-11-06 22:18:40 -0600

    done req but setting up the pop up

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+  let nameEQ = name + "=";
+  let cookiesArray = document.cookie.split(";");
+  for (let i = 0; i < cookiesArray.length; i++) {
+    let cookie = cookiesArray[i].trim();
+    if (cookie.indexOf(nameEQ) === 0) {
+      return cookie.substring(nameEQ.length);
+    }
+  }
+  return null;
+}
+
+
 function showTopBanner() {
 
   var banner = document.getElementById("top-banner");
@@ -32,13 +55,17 @@ function closeTopBanner() {
 }
 
 //session storage feature
-function closeFooterBanner() {
+ function closeFooterBanner() {
   document.getElementById("footer-banner").classList.add("hide");
-   setCookie("footerClosed", "true", 3); 
+  setCookie("footerBannerClosed", "true", 3); 
+}
    // cookie will last 3 days
 }
 
+
       // Adding prevent automatic loading 
+//Preventing things from loading again if user already closed them before
+// get it corrected from one of my friend
 // check if top banner was closed in this session
 if (!sessionStorage.getItem("topBannerClosed")) {
   setTimeout(showTopBanner, 2000);
@@ -61,6 +88,9 @@ document
   .getElementById("footer-banner")
   .addEventListener("click", closeFooterBanner);
 
-setTimeout(showFooterBanner, 1000);
-setTimeout(showTopBanner, 2000);
-setTimeout(showModal, 4000);
+
+
+// Clear All Stored Data Button
+document.getElementById("clearDataBtn").addEventListener("click", function () {
+  // clear session storage (for top banner)
+  sessionStorage.clear();
